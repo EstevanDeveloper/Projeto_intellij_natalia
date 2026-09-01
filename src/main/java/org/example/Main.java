e46117b1bee7acf2bdd94628344b548e79794334
RENAN ESTEVAN DA SILVA BIANQUI <renan.bianqui@senaisp.edu.br>
2026-09-01 14:03:51 -0300

    primeiro commit no intelli

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        IO.println(String.format("digite a sua idade: "));
+        Scanner sc = new Scanner (System.in);
+        int idade = sc.nextInt();
+
+        if (idade >= 18){
+
+            System.out.println("Maior idade");
+        } else {
+            System.out.println("menor idade");
+        }
+
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+
+
+    }
+}
