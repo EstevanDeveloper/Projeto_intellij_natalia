174edc702ce693b6573d5837336d4c2b92263160
Renan <renan.estevan@yahoo.com>
2026-09-10 15:44:00 -0300

    atividade 10

---
+++ meuPrimeiroProjetoSenai-main/meuPrimeiroProjetoSenai/untitled/src/main/java/org/example/atividade10.java
@@ -0,0 +1,22 @@
+package org.example;
+import java.util.Scanner;
+
+class Main10 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Informe o salário do cliente: ");
+        double salario = entrada.nextDouble();
+
+        System.out.print("Informe o valor da parcela desejada: ");
+        double parcela = entrada.nextDouble();
+
+        if (parcela <= salario * 0.30) {
+            System.out.println("Empréstimo aprovado.");
+        } else {
+            System.out.println("Empréstimo recusado.");
+        }
+
+        entrada.close();
+    }
+}
